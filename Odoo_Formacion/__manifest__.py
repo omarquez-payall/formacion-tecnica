3e563116e9d526f70563d05992e5d55a685fc28b
Odoo Master <omarquezpayall@gmail.com>
2021-09-06 18:45:21 +0000

    [Add] Agregar esqueleto inicial del modulo

---
+++ Odoo_Formacion/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Formacion Tecnica',
+    
+    'summary': """ Resumen Odoo Formacion Tecnica """,
+    
+    'description': """
+        Formacion Tecnica de Odoo
+        - Aprender
+        - Disciplina
+        - Constancia
+    """,
+    
+    'author': 'SIUUU',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Entrenamiento',
+    
+    'version': '0.0.1',
+    
+    'depends': ['base'],
+    
+    'data':[
+        
+    ],
+    
+    'demo': [
+        
+    ],
+}
